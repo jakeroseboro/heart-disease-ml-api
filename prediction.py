0d098b79f2b03fc4fcf83abfd6a81013509c770b
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2021-12-09 22:51:42 -0600

    transform the data

--- prediction.py
+++ prediction.py
@@ -4,4 +4,11 @@ import pandas as pd
 def predict_heart_disease(data):
     df = pd.read_csv('heart.csv')
     df = df.drop("Cholesterol", 1)
+    df['Sex'] = df['Sex'].replace(['F', 'M'], [0,  1])
+    df["ChestPainType"] = df["ChestPainType"].replace(["TA", "ATA", "NAP", "ASY"], [0,1,2,3])
+    df["RestingECG"] = df["RestingECG"].replace(["Normal", "ST", "LVH"],[0,1,2])
+    df["ExerciseAngina"] = df["ExerciseAngina"].replace(["Y", "N"], [0,1])
+    df["ST_Slope"] = df["ST_Slope"].replace(["Up", "Flat", "Down"], [0,1,2])
+
     print(df)
+    return "HEY"
