562c683aae33887f2702a9258a9f80f8f8acd4ba
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2021-12-20 16:27:51 -0600

    Update prediction.py
    get best params for RFC, round data

--- prediction.py
+++ prediction.py
@@ -1,5 +1,6 @@
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
 
 
@@ -20,10 +21,41 @@ def predict_heart_disease(data):
     x = df.drop('HeartDisease', 1)
 
     # Generate test and training data
-    x_train, x_test, y_train, y_test = train_test_split(x, df['HeartDisease'], test_size=0.25, random_state=0)
+    x_train, x_test, y_train, y_test = train_test_split(x, df['HeartDisease'], test_size=0.2, random_state=0)
 
-    clf = RandomForestClassifier()
+    clf = RandomForestClassifier(n_estimators=234, min_samples_split=10, min_samples_leaf=2, max_features='sqrt',
+                                 max_depth=80, bootstrap=True)
 
     clf.fit(x_train, y_train)
-    y_pred = clf.predict_proba(new_x)[:,1]
-    return str(y_pred)
+    y_pred = clf.predict_proba(new_x)[:,1][0]
+    return str(round(y_pred, 4))
+
+
+def get_best_params(x_train, y_train):
+
+    n_estimators = [int(x) for x in np.linspace(start=103, stop=300, num=10)]
+    # Number of features to consider at every split
+    max_features = ['auto', 'sqrt']
+    # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
+    max_depth.append(None)
+    # Minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 4]
+    # Method of selecting samples for training each tree
+    bootstrap = [True, False]
+    # Create the random grid
+    random_grid = {'n_estimators': n_estimators,
+                   'max_features': max_features,
+                   'max_depth': max_depth,
+                   'min_samples_split': min_samples_split,
+                   'min_samples_leaf': min_samples_leaf,
+                   'bootstrap': bootstrap}
+
+    rf = RandomForestClassifier()
+    rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=100, cv=3, verbose=2,
+                                   random_state=42, n_jobs=-1)
+    # Fit the random search model
+    rf_random.fit(x_train, y_train)
+    print(rf_random.best_params_)
